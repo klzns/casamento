5a3be9090191e625806682235103204c17dfb4bf
Elisa <elisamussumeci@gmail.com>
2018-09-07 15:34:04 -0300

    Adiciona informacoes de transporte

--- src/components/InfoLocal.js
+++ src/components/InfoLocal.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
 import PropTypes from 'prop-types'
 
+const Info = ({ children }) =>
+  <Fragment>
+    <strong style={{ color: 'blue' }}>{children}</strong>
+    <br />
+  </Fragment>
+
 class InfoLocal extends Component {
-  render () {
+  render() {
     return (
       <p>
-        <strong style={{ color: 'blue' }}>Local:</strong> <br />
+        <Info>Local:</Info>
         <a href="https://goo.gl/maps/SMMfdnm5DG92">Espaço Arcádia</a> <br />
         Estr. Paiva Muniz, 750 - Guaratiba <br />
         Rio de Janeiro - RJ, 23020-010 <br />
-        <strong style={{ color: 'blue' }}>Dia e hora:</strong> <br />
+        <Info>Dia e hora:</Info>
         08/09/2018 às 16h <br />
-        <strong style={{ color: 'blue' }}>Traje:</strong> <br />
+        (demora 1h15 pra chegar da Zona Sul até lá)<br />
+        <Info>Traje:</Info>
         Vai bem bonitinho <br />
-        <strong style={{ color: 'blue' }}>Importante!</strong> <br />
-        Levar o convite para apresentar na entrada!
+        <Info>Transporte:</Info>
+        As vans contratadas estão lotadas. <br />
+        Uma boa opção é ir de Uber e agendar a volta no Uber pras 22h40, horário do fim da festa. <br />
+        <Info>Importante!</Info>
+        Levar o convite para apresentar na entrada!<br />
       </p>
     )
   }
